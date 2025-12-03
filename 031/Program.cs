bad6202468e692c2065da1a70961c55bb51268b6
Per Jahn <perjahn@gmail.com>
2025-12-03 07:09:05 +0100

    3rd

---
+++ 031/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var sum = FindMaxJoltage(rows);
+
+        Console.WriteLine(sum);
+    }
+
+    static int FindMaxJoltage(string[] batteries)
+    {
+        var sum = 0;
+
+        foreach (var bank in batteries)
+        {
+            var firstDigit = -1;
+            var secondDigit = -1;
+
+            for (var c = '9'; c >= '0'; c--)
+            {
+                var index = bank[..^1].IndexOf(c);
+                if (index >= 0)
+                {
+                    firstDigit = index;
+                    break;
+                }
+            }
+            for (var c = '9'; c >= '0'; c--)
+            {
+                var index = bank[(firstDigit + 1)..].LastIndexOf(c);
+                if (index >= 0)
+                {
+                    secondDigit = firstDigit + 1 + index;
+                    break;
+                }
+            }
+
+            sum += int.Parse($"{bank[firstDigit]}{bank[secondDigit]}");
+        }
+
+        return sum;
+    }
+}
---
+++ 032/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var sum = FindMaxJoltage(rows);
+
+        Console.WriteLine(sum);
+    }
+
+    static long FindMaxJoltage(string[] batteries)
+    {
+        long sum = 0;
+
+        foreach (var bank in batteries)
+        {
+            var digits = new int[12];
+
+            for (var i = 0; i < 12; i++)
+            {
+                for (var c = '9'; c >= '0'; c--)
+                {
+                    var s = bank[..^(11 - i)];
+                    var index = i == 0 ? s.IndexOf(c) : s.IndexOf(c, digits[i - 1] + 1);
+
+                    if (index >= 0)
+                    {
+                        digits[i] = index;
+                        break;
+                    }
+                }
+            }
+
+            sum += long.Parse($"{string.Join(string.Empty, digits.Select(d => bank[d]))}");
+        }
+
+        return sum;
+    }
+}
