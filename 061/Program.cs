4c61aac86f5b6feb135e865343bb8b23b3868df2
Per Jahn <perjahn@gmail.com>
2025-12-06 07:19:37 +0100

    6th

---
+++ 061/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var total = GetTotal(rows);
+
+        Console.WriteLine(total);
+    }
+
+    static long GetTotal(string[] rows)
+    {
+        List<long[]> values = [];
+
+        for (var i = 0; i < rows.Length - 1; i++)
+        {
+            long[] nums = [.. rows[i]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)];
+
+            values.Add(nums);
+        }
+
+        char[] operators = [.. rows[^1]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select( o => o[0])];
+
+        long total = 0;
+
+        for (var i = 0; i < values[0].Length; i++)
+        {
+            long colsum = values[0][i];
+
+            for (var j = 1; j < values.Count; j++)
+            {
+                switch (operators[i])
+                {
+                    case '+':
+                        colsum += values[j][i];
+                        break;
+                    case '*':
+                        colsum *= values[j][i];
+                        break;
+                    default:
+                        Console.WriteLine("Unknown operator");
+                        break;
+                }
+            }
+
+            total += colsum;
+        }
+
+        return total;
+    }
+}
---
+++ 062/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var total = GetTotal(rows);
+
+        Console.WriteLine(total);
+    }
+
+    static long GetTotal(string[] rows)
+    {
+        rows = Rotate(rows);
+
+        long total = 0;
+        long sum = 0;
+        var op = ' ';
+
+        for (var i = 0; i < rows.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(rows[i]))
+            {
+                continue;
+            }
+            else if (rows[i][^1] is '*' or '+')
+            {
+                op = rows[i][^1];
+                sum = long.Parse(rows[i][..^1]);
+            }
+            else
+            {
+                if (op == '*')
+                {
+                    sum *= long.Parse(rows[i]);
+                }
+                else if (op == '+')
+                {
+                    sum += long.Parse(rows[i]);
+                }
+            }
+
+            if (i == rows.Length - 1 || string.IsNullOrWhiteSpace(rows[i + 1]))
+            {
+                total += sum;
+                sum = 0;
+            }
+        }
+
+        return total;
+    }
+
+    static string[] Rotate(string[] rows)
+    {
+        var rotated = new string[rows[0].Length];
+
+        for (var i = 0; i < rows[0].Length; i++)
+        {
+            rotated[i] = string.Join(string.Empty, rows.Select(r => r[i]));
+        }
+
+        return rotated;
+    }
+}
