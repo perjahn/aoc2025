f341032102f8fa4307b74ce0e581b317cf16d6e9
Per Jahn <perjahn@gmail.com>
2025-12-07 15:35:43 +0100

    7th

---
+++ 071/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var splits = GetSplits(rows);
+
+        Console.WriteLine(splits);
+    }
+
+    static long GetSplits(string[] rows)
+    {
+        char[][] charrows = [.. rows.Select(r => r.ToCharArray())];
+
+        long splits = 0;
+
+        for (var row = 0; row < charrows.Length - 1; row++)
+        {
+            for (var i = 0; i < charrows[row].Length; i++)
+            {
+                if (i < charrows.Length - 1 && charrows[row][i] is 'S' or '|')
+                {
+                    if (charrows[row + 1][i] == '^')
+                    {
+                        charrows[row + 1][i - 1] = '|';
+                        charrows[row + 1][i + 1] = '|';
+                        splits++;
+                    }
+                    if (charrows[row + 1][i] == '.')
+                    {
+                        charrows[row + 1][i] = '|';
+                    }
+                }
+            }
+        }
+
+        return splits;
+    }
+}
---
+++ 072/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var rows = File.ReadAllLines("input.txt");
+
+        var splits = GetTotalSplits(rows);
+
+        Console.WriteLine(splits);
+    }
+
+    static long GetTotalSplits(string[] rows)
+    {
+        long splits = 0;
+
+        for (var i = 0; i < rows[0].Length; i++)
+        {
+            if (rows[0][i] == 'S')
+            {
+                splits += GetSplits(rows, i, 0);
+            }
+        }
+
+        return splits;
+    }
+
+    static readonly Dictionary<(int x, int y), long> memoization = [];
+
+    static long GetSplits(string[] rows, int x, int y)
+    {
+        if (memoization.TryGetValue((x, y), out var timelines))
+        {
+            return timelines;
+        }
+
+        if (y == rows.Length - 1)
+        {
+            memoization[(x, y)] = 1;
+            return 1;
+        }
+
+        if (rows[y + 1][x] == '.')
+        {
+            var splits = GetSplits(rows, x, y + 1);
+            memoization[(x, y)] = splits;
+            return splits;
+        }
+
+        if (rows[y + 1][x] == '^')
+        {
+            var splits = GetSplits(rows, x - 1, y + 1) + GetSplits(rows, x + 1, y + 1);
+            memoization[(x, y)] = splits;
+            return splits;
+        }
+
+        Console.WriteLine($"Unexpected character ({x},{y + 1}): '{rows[y + 1][x]}'");
+        memoization[(x, y)] = 1;
+        return 1;
+    }
+}
